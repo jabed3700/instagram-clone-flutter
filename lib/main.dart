64ce079939b256f5f7712681d093b5d5b6c804d6
tamim008bd <tamim008bd@gmail.com>
2022-01-27 16:18:45 +0600

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
@@ -5,7 +7,20 @@ import 'package:instagram_flutter/utils/colors.dart';
 
 import 'responsive/responsive_layout_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyBLTPh29lieMIzNx9jdz6zhV1Hr1u-kxO8",
+      appId: "1:626459613098:web:493f5850c26d19c50abdd3",
+      messagingSenderId: "626459613098",
+      projectId: "instagram-clone-fd025",
+      storageBucket: "instagram-clone-fd025.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
